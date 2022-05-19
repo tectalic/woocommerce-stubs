d55bd8f882f1ad03b13e7bcd9a1e26a791b9969c
James Collins <james@om4.com.au>
2022-05-19 10:49:44 +0800

    Remove WC Admin from Packages Stubs, its now part of WC Core as of WC 6.5.0
    As per https://developer.woocommerce.com/2022/05/10/woocommerce-6-5-released/:
    
    WooCommerce Admin: All of WooCommerce Admin’s code and assets have been merged into Core and the feature plugin has been deprecated. These are the folders that were moved and the new location of the contained files.
    
        packages/woocommerce-admin/src -> src/Admin
        packages/woocommerce-admin/src-internal -> src/Internal
        packages/woocommerce-admin/includes -> includes/react-admin
        packages/woocommerce-admin/dist -> assets/client/admin
        packages/woocommerce-admin/images -> assets/images

--- finder-packages.php
+++ finder-packages.php
@@ -21,17 +21,5 @@ return \StubsGenerator\Finder::create()
             ->depth('< 1')
             ->path('functions.php')
     )
-    // WC Admin "includes".
-    ->append(
-        \StubsGenerator\Finder::create()
-            ->in(['source/woocommerce/packages/woocommerce-admin/includes'])
-            ->in(['source/woocommerce/packages/woocommerce-admin/src'])
-            ->in(['source/woocommerce/packages/woocommerce-admin/src-internal'])
-            // Email templates.
-            ->notPath('emails')
-            // Update functions.
-            ->notPath('wc-admin-update-functions.php')
-            ->sortByName(true)
-    )
     ->sortByName(true)
 ;
