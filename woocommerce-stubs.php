64179d03806d4ce4d2b8717b2658db9d6cffca2c
Csaba Maulis <csaba@om4.com.au>
2022-10-05 16:52:49 +0800

    Generate stubs for WooCommerce 6.6.0

--- woocommerce-stubs.php
+++ woocommerce-stubs.php
@@ -55272,6 +55272,7 @@ namespace {
         public function _topic_count_text($count)
         {
         }
+        // @codingStandardsIgnoreEnd
     }
     /**
      * Widget products.
@@ -72440,6 +72441,78 @@ namespace Automattic\WooCommerce\Database\Migrations {
         }
     }
 }
+namespace Automattic\WooCommerce\Internal\Admin {
+    /**
+     * CouponsMovedTrait trait.
+     */
+    trait CouponsMovedTrait
+    {
+        /**
+         * The GET query key for the legacy menu.
+         *
+         * @var string
+         */
+        protected static $query_key = 'legacy_coupon_menu';
+        /**
+         * The key for storing an option in the DB.
+         *
+         * @var string
+         */
+        protected static $option_key = 'wc_admin_show_legacy_coupon_menu';
+        /**
+         * Get the URL for the legacy coupon management.
+         *
+         * @return string The unescaped URL for the legacy coupon management page.
+         */
+        protected static function get_legacy_coupon_url()
+        {
+        }
+        /**
+         * Get the URL for the coupon management page.
+         *
+         * @param array $args Additional URL query arguments.
+         *
+         * @return string
+         */
+        protected static function get_coupon_url($args = [])
+        {
+        }
+        /**
+         * Get the new URL for managing coupons.
+         *
+         * @param string $page The management page.
+         *
+         * @return string
+         */
+        protected static function get_management_url($page)
+        {
+        }
+        /**
+         * Get the WC Admin path for the marking page.
+         *
+         * @return string
+         */
+        protected static function get_marketing_path()
+        {
+        }
+        /**
+         * Whether we should display the legacy coupon menu item.
+         *
+         * @return bool
+         */
+        protected static function should_display_legacy_menu()
+        {
+        }
+        /**
+         * Set whether we should display the legacy coupon menu item.
+         *
+         * @param bool $display Whether the menu should be displayed or not.
+         */
+        protected static function display_legacy_menu($display = false)
+        {
+        }
+    }
+}
 namespace Automattic\WooCommerce {
     /**
      * Packages class.
@@ -72531,6 +72604,7 @@ namespace Automattic\WooCommerce\Proxies {
         public function apply_filters($tag, $value, ...$parameters)
         {
         }
+        // TODO: Add the rest of the actions and filters related methods.
     }
     /**
      * Proxy class to access legacy WooCommerce functionality.
