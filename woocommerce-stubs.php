a5f3472f6a887275903b2283e122e70388303328
Csaba Maulis <csaba@om4.com.au>
2022-10-05 16:55:11 +0800

    Generate stubs for WooCommerce 6.7.0-beta.2

--- woocommerce-stubs.php
+++ woocommerce-stubs.php
@@ -54043,10 +54043,20 @@ namespace {
          * Note: the event request won't be made if $properties has a member called `error`.
          *
          * @param string $event_name The name of the event.
-         * @param array  $properties Custom properties to send with the event.
+         * @param array  $event_properties Custom properties to send with the event.
          * @return bool|WP_Error True for success or WP_Error if the event pixel could not be fired.
          */
-        public static function record_event($event_name, $properties = array())
+        public static function record_event($event_name, $event_properties = array())
+        {
+        }
+        /**
+         * Get all properties for the event including filtered and identity properties.
+         *
+         * @param string $event_name Event name.
+         * @param array  $event_properties Event specific properties.
+         * @return array
+         */
+        public static function get_properties($event_name, $event_properties)
         {
         }
     }
